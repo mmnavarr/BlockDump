40325551d0ec649d38f822048ff749dfebc4d49f
malcolmnavarro <mmnavarr@live.com>
2015-04-20 23:48:18 -0400

    Initial Commit

---
+++ BlockDump/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlockDump
+//
+//  Created by Malcolm Navarro on 4/20/15.
+//  Copyright (c) 2015 cyberplays. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
