bf99731ea08fd9c05a2a5b66cfaaf7c5a96fb772
hagan116 <sohagan@syr.edu>
2015-05-06 21:25:11 -0400

    update

--- BlockDump/PlayViewController.m
+++ BlockDump/PlayViewController.m
@@ -24,6 +24,11 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     CGRect bounds = [self.view bounds];
+    secondCount = 0;
+    
+    
+    //initialize arrays
+    spriteViews = [[NSMutableArray alloc] init];
     
     spriteNames = [[NSMutableArray alloc] init];
     [spriteNames addObject:@"triangle_sprite"];
@@ -38,8 +43,15 @@
     CGFloat h = CGRectGetHeight(bounds);
     
     //height and width of the game grid for sprite spawning
-    CGFloat gridW = w - 50.0f;
-    CGFloat gridH = h - 100.0f;
+    gridW = w - 50.0f;
+    gridH = h - 100.0f;
+    toW = gridW/2;
+    toH = gridH/2;
+    topLeft = CGPointMake(50.0, 150.0); //top left corner of the grid
+    topRight = CGPointMake(gridW + 50, 150);//top right
+    bottomLeft = CGPointMake(50, gridH + 150);//etc
+    bottomRight = CGPointMake(gridW + 50, gridH + 150);
+    
     
     //start the timer
     timer = [NSTimer scheduledTimerWithTimeInterval:1
@@ -108,12 +120,96 @@
 
 - (void) updateLabel:(NSTimer*)theTimer
 {
+    secondCount++;
     timeLeft = timeLeft - 1;
     if (timeLeft <= 0){
         //game ends
     }
+    //generate/add a sprite every two seconds
+    if (secondCount == 2){
+    int spriteRandom = arc4random_uniform(4);//pick a random sprite
+    int sideRandom = arc4random_uniform(4);//pick a random side
+    NSString * spriteName = [[NSString alloc] init];
+        switch (sideRandom){
+            case (0):{//bottom side
+                switch (spriteRandom){
+                    case (0):{
+                        spriteName = [spriteNames objectAtIndex:0];
+                        UIImage *spriteImg = [UIImage imageNamed:@"spriteName"];
+                        UIImageView *spriteView = [[UIImageView alloc] initWithImage:spriteImg];
+                        
+                        
+                    }
+                    case (1):{
+                        spriteName = [spriteNames objectAtIndex:1];
+                        UIImage *spriteImg = [UIImage imageNamed:@"spriteName"];
+                        UIImageView *spriteView = [[UIImageView alloc] initWithImage:spriteImg];
+                    }
+                    case (2):{
+                        spriteName = [spriteNames objectAtIndex:2];
+                        UIImage *spriteImg = [UIImage imageNamed:@"spriteName"];
+                        UIImageView *spriteView = [[UIImageView alloc] initWithImage:spriteImg];
+                    }
+                    case (3):{
+                        spriteName = [spriteNames objectAtIndex:3];
+                        UIImage *spriteImg = [UIImage imageNamed:@"spriteName"];
+                        UIImageView *spriteView = [[UIImageView alloc] initWithImage:spriteImg];
+                    }
+                }
+            }
+            case (1):{//left side
+                switch (spriteRandom){
+                    case (0):{
+                        spriteName = [spriteNames objectAtIndex:0];
+                    }
+                    case (1):{
+                        spriteName = [spriteNames objectAtIndex:1];
+                    }
+                    case (2):{
+                        spriteName = [spriteNames objectAtIndex:2];
+                    }
+                    case (3):{
+                        spriteName = [spriteNames objectAtIndex:3];
+                    }
+                }
+            }
+            case (2):{//top side
+                switch (spriteRandom){
+                    case (0):{
+                        spriteName = [spriteNames objectAtIndex:0];
+                    }
+                    case (1):{
+                        spriteName = [spriteNames objectAtIndex:1];
+                    }
+                    case (2):{
+                        spriteName = [spriteNames objectAtIndex:2];
+                    }
+                    case (3):{
+                        spriteName = [spriteNames objectAtIndex:3];
+                    }
+                }
+            }
+            case (3):{//right side
+                switch (spriteRandom){
+                    case (0):{
+                        spriteName = [spriteNames objectAtIndex:0];
+                    }
+                    case (1):{
+                        spriteName = [spriteNames objectAtIndex:1];
+                    }
+                    case (2):{
+                        spriteName = [spriteNames objectAtIndex:2];
+                    }
+                    case (3):{
+                        spriteName = [spriteNames objectAtIndex:3];
+                    }
+                }
+            }
+        }
+    }
     _timeLabel.text = [NSString stringWithFormat:@"Time: %is", (int)timeLeft];
 }
+
 
 /*
 #pragma mark - Navigation
