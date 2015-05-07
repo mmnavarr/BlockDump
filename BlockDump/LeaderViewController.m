9085afcbe316970ca31b48dc60fd8a352b1e6ca7
malcolmnavarro <mmnavarr@live.com>
2015-05-07 14:20:23 -0400

    sheesh

--- BlockDump/LeaderViewController.m
+++ BlockDump/LeaderViewController.m
@@ -28,13 +28,20 @@
     
     Player *p1 = [[Player alloc] initWithName:(NSString *)@"Malcolm Navarro" andScore:(NSInteger) 203050];
     Player *p2 = [[Player alloc] initWithName:(NSString *)@"Eric Bledsoe" andScore:(NSInteger) 198400];
-    Player *p3 = [[Player alloc] initWithName:(NSString *)@"Sam Hagan" andScore:(NSInteger) 150900];
-    Player *p4 = [[Player alloc] initWithName:(NSString *)@"Ron Swanson" andScore:(NSInteger) 53000];
-    Player *p5 = [[Player alloc] initWithName:(NSString *)@"Bill Belichick" andScore:(NSInteger) 2000];
+    Player *p3 = [[Player alloc] initWithName:(NSString *)@"Sam Hagan" andScore:(NSInteger) 160900];
+    Player *p4 = [[Player alloc] initWithName:(NSString *)@"Ron Swanson" andScore:(NSInteger) 160300];
+    Player *p5 = [[Player alloc] initWithName:(NSString *)@"Jim Fisher" andScore:(NSInteger) 151900];
+    Player *p6 = [[Player alloc] initWithName:(NSString *)@"Bill Nye" andScore:(NSInteger) 139210];
+    Player *p7 = [[Player alloc] initWithName:(NSString *)@"Tom Brady" andScore:(NSInteger) 138150];
+    Player *p8 = [[Player alloc] initWithName:(NSString *)@"Aaron Hernandez" andScore:(NSInteger) 121000];
+    Player *p9 = [[Player alloc] initWithName:(NSString *)@"Wilt Chamberlain" andScore:(NSInteger) 100850];
+    Player *p10 = [[Player alloc] initWithName:(NSString *)@"Barack Obama" andScore:(NSInteger) 99330];
+    Player *p11 = [[Player alloc] initWithName:(NSString *)@"Albert Einstein" andScore:(NSInteger) 73050];
+    Player *p12 = [[Player alloc] initWithName:(NSString *)@"Rose Mary" andScore:(NSInteger) 52000];
     
     
     //ADD PLAYERS TO ARRAY
-    _players = [NSMutableArray arrayWithObjects: p1, p2, p3, p4, p5, nil];
+    _players = [NSMutableArray arrayWithObjects: p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, nil];
 }
 
 
